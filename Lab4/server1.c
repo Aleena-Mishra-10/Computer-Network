5f091b850692be5ffcb40508c3f8a26d5c0fb53e
Aleena MIshra <56436393+Aleena-Mishra-10@users.noreply.github.com>
2020-10-23 13:34:31 +0530

    Create server1.c

---
+++ Lab4/server1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+int main()
+{
+int ss,sb,nbytes,i;
+ss=socket(AF_INET,SOCK_DGRAM,0);
+struct sockaddr_in servaddr;
+servaddr.sin_family=AF_INET;
+servaddr.sin_port=ntohs(6011);
+
+servaddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+
+bind(ss,(struct sockaddr *)&servaddr,sizeof(struct sockaddr));
+struct sockaddr_in tempsendaddr;
+char buf[100];
+int len=sizeof(struct sockaddr);
+recvfrom(ss,buf,100,0,(struct sockaddr *)&tempsendaddr,&len);
+printf("Receive from: %s",buf);
+char buf1[100];
+for(i=0;buf[i]!='\0';i++)
+{
